f8f5eb6726c486bf3f7444d3227d01046534f607
joelbs98 <joelbarbasa98@gmail.com>
2026-01-08 20:13:09 -0500

    Clase GET y POST

---
+++ get_post/recibir_get.php
@@ -0,0 +1,26 @@
+<?php
+//RECIBIR DATOS CON GET
+$curso = $_GET["curso"] ?? "No definido";
+//$GET["curso] = "desarrollo_web
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recibir GET</title>
+</head>
+
+<body>
+    <h1>DATOS RECIBIDOS CON GET</h1>
+    <p>Curso seleccionado: </p>
+    <strong><?= $curso ?></strong>
+
+    <br><br>
+    <a href="index.php">Volver</a>
+
+</body>
+
+</html>
